e27732fbc26fbbf76b14ef0e2462bd722ad5260c
Shkarin Sergey <kouki.sergey@gmail.com>
2020-06-24 17:14:18 +0300

    Добавлен прототип краулера

---
+++ crawler/gamemag/pipelines.py
@@ -0,0 +1,3 @@
+class GamemagPipeline:
+    def process_item(self, item, spider):
+        return item
---
+++ crawler/gamemag/settings.py
@@ -0,0 +1,4 @@
+BOT_NAME = 'gamemag'
+SPIDER_MODULES = ['gamemag.spiders']
+NEWSPIDER_MODULE = 'gamemag.spiders'
+ROBOTSTXT_OBEY = True
